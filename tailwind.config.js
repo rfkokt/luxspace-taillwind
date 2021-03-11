c4f6cceb483d35e3f4ca8ecb70efeb1ef4f0789d
rfkokt <rifkiokta105@gmail.com>
2021-03-11 23:54:02 +0700

    update Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: { ...colors.coolGray, 100: "#F9F9F9", 200: "#F1F1F1" },
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
